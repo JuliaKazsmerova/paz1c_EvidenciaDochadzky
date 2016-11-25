665a85c9f78a8887a58e902769d71df701c25f18
PatrikSakac <pato.sakac@gmail.com>
2016-11-25 11:51:05 +0100

    spatButton v ZmazatZamestnanca, nefunkcne oznacovanie osoby

--- src/main/java/com/paz1c/gui/zamestnanec/MazanyZamestnanecTableModel.java
+++ src/main/java/com/paz1c/gui/zamestnanec/MazanyZamestnanecTableModel.java
@@ -6,7 +6,7 @@ import javax.swing.table.AbstractTableModel;
 public class MazanyZamestnanecTableModel extends AbstractTableModel{
     
     private com.paz1c.manager.ZamestnanecManager zamestnanecManager = new com.paz1c.manager.DefaultZamestnanecManager();
-    private static final String[] NAZVY_STLPCOV = { "Meno", "Priezvisko", "Zameranie" };
+    private static final String[] NAZVY_STLPCOV = { "Meno", "Priezvisko", "Zameranie", "Označenie" };
     private static final int POCET_STLPCOV = NAZVY_STLPCOV.length;
     private Long id;
     
@@ -30,16 +30,35 @@ public class MazanyZamestnanecTableModel extends AbstractTableModel{
                 return zamestnanec.getPriezvisko();
             case 2:
                 return zamestnanec.getZameranie();
+            case 3:
+                return false;
             default:
                 return "???";
         }
+        
     }
     
+        @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        if (columnIndex == 3) {
+            return Boolean.class;
+        }
+        
+        return super.getColumnClass(columnIndex);
+    }
+
+    @Override
+    public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
+        super.setValueAt(aValue, rowIndex, columnIndex); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+
     @Override
     public String getColumnName(int columnIndex) {
         return NAZVY_STLPCOV[columnIndex];
     }
-    
+
     void najdiPodlaId(Long id) {
         this.id = id;
         fireTableDataChanged();
--- src/main/java/com/paz1c/gui/zamestnanec/ZmazatZamestnanca.java
+++ src/main/java/com/paz1c/gui/zamestnanec/ZmazatZamestnanca.java
@@ -1,6 +1,10 @@
 package com.paz1c.gui.zamestnanec;
 
 import com.paz1c.other.Zamestnanec;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JOptionPane;
 import javax.swing.table.DefaultTableModel;
 
 public class ZmazatZamestnanca extends javax.swing.JPanel {
@@ -16,7 +20,8 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
     
         private void aktualizovatZamestnancov(Long ID) {
         // ziskaj model
-        MazanyZamestnanecTableModel model = (MazanyZamestnanecTableModel) zamestnanecNaMazanieTable.getModel();        
+        MazanyZamestnanecTableModel model = (MazanyZamestnanecTableModel) zamestnanecNaMazanieTable.getModel(); 
+        zamestnanecNaMazanieTable.getTableHeader().setUI(null);
         // pridaj zamestnanca
         model.najdiPodlaId(ID);
     }
@@ -37,6 +42,7 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
         potvrditButton = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         zamestnanecNaMazanieTable = new javax.swing.JTable();
+        spatButton = new javax.swing.JButton();
 
         setBackground(new java.awt.Color(255, 250, 226));
 
@@ -57,9 +63,26 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
         overovaciaOtazkaLabel.setText("Naozaj si prajete zmazať túto osobu?");
 
         potvrditButton.setText("Potvrdiť");
+        potvrditButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                potvrditButtonActionPerformed(evt);
+            }
+        });
 
         zamestnanecNaMazanieTable.setModel(new MazanyZamestnanecTableModel());
+        zamestnanecNaMazanieTable.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                zamestnanecNaMazanieTableMouseClicked(evt);
+            }
+        });
         jScrollPane2.setViewportView(zamestnanecNaMazanieTable);
+
+        spatButton.setText("Späť");
+        spatButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                spatButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -70,7 +93,8 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(overovaciaOtazkaLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 459, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(spatButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(potvrditButton))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addComponent(IDZamestnancaTextField)
@@ -99,21 +123,52 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(potvrditButton)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(potvrditButton)
+                    .addComponent(spatButton))
                 .addContainerGap())
         );
     }// </editor-fold>//GEN-END:initComponents
 
     private void zmazatButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_zmazatButtonActionPerformed
-        Long zadaneID = Long.parseLong(IDZamestnancaTextField.getText());
-        aktualizovatZamestnancov(zadaneID);
+        if(IDZamestnancaTextField.getText().equals("")){
+            JOptionPane.showMessageDialog(parentJForm, "Nezadali ste ID zamestnanca!", "Upozornenie", JOptionPane.ERROR_MESSAGE);
+        }else{
+            Long zadaneID = Long.parseLong(IDZamestnancaTextField.getText());
+            aktualizovatZamestnancov(zadaneID);
+ 
+            overovaciaOtazkaLabel.setVisible(true);  
+            jScrollPane2.setVisible(true);
+            potvrditButton.setVisible(true);
+        }
         
-        
-        
-        overovaciaOtazkaLabel.setVisible(true);  
-        jScrollPane2.setVisible(true);
-        potvrditButton.setVisible(true);
+       
     }//GEN-LAST:event_zmazatButtonActionPerformed
+
+    private void spatButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_spatButtonActionPerformed
+        parentJForm.otvorOkno("tableZamestnanci");
+        IDZamestnancaTextField.setText("");
+        overovaciaOtazkaLabel.setVisible(false);  
+            jScrollPane2.setVisible(false);
+            potvrditButton.setVisible(false);
+    }//GEN-LAST:event_spatButtonActionPerformed
+
+    private void potvrditButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_potvrditButtonActionPerformed
+        
+    }//GEN-LAST:event_potvrditButtonActionPerformed
+
+    private void zamestnanecNaMazanieTableMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_zamestnanecNaMazanieTableMouseClicked
+        int row = zamestnanecNaMazanieTable.getSelectedRow();
+        int column = zamestnanecNaMazanieTable.convertColumnIndexToModel(zamestnanecNaMazanieTable.getSelectedColumn());
+            MazanyZamestnanecTableModel model = (MazanyZamestnanecTableModel) zamestnanecNaMazanieTable.getModel(); 
+            model.setValueAt(Boolean.TRUE, row, column);
+        /*
+        if (zamestnanecNaMazanieTable.getValueAt(row, column) == Boolean.FALSE){
+       
+        }
+        */
+         
+    }//GEN-LAST:event_zamestnanecNaMazanieTableMouseClicked
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -123,6 +178,7 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel overovaciaOtazkaLabel;
     private javax.swing.JButton potvrditButton;
+    private javax.swing.JButton spatButton;
     private javax.swing.JTable zamestnanecNaMazanieTable;
     private javax.swing.JButton zmazatButton;
     // End of variables declaration//GEN-END:variables
