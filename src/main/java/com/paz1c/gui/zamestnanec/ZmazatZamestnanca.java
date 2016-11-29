04bfa194d57260e66ec7332d64fc1e89be9c6444
PatrikSakac <pato.sakac@gmail.com>
2016-11-29 14:31:12 +0100

    pridana zmena jazyka pre Zametnancov

--- src/main/java/com/paz1c/gui/zamestnanec/ZmazatZamestnanca.java
+++ src/main/java/com/paz1c/gui/zamestnanec/ZmazatZamestnanca.java
@@ -3,17 +3,22 @@ package com.paz1c.gui.zamestnanec;
 import com.paz1c.constants.Nastavenia;
 import com.paz1c.other.Zamestnanec;
 import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Map;
 import javax.swing.JOptionPane;
 
 public class ZmazatZamestnanca extends javax.swing.JPanel {
 
     private SpravaZamestnancov parentJForm;
     private boolean potvrdene;
-    MazanyZamestnanecTableModel model;
-    Zamestnanec zamestnanec;
+    private MazanyZamestnanecTableModel model;
+    private Zamestnanec zamestnanec;
+    private Map<String,String> mapaString = new HashMap<>();
+   
     
     public ZmazatZamestnanca() {
         initComponents();
+        nastavJazyk();
         model = (MazanyZamestnanecTableModel) zamestnanecNaMazanieTable.getModel(); 
         zamestnanecNaMazanieTable.getTableHeader().setUI(null);
         overovaciaOtazkaLabel.setVisible(false);
@@ -24,14 +29,67 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
 
     public void setParent(SpravaZamestnancov parent){
         this.parentJForm = parent;
+        zmenaJazykaPanel1.setParentSpravaZamestnancov(parentJForm);
+    }
+    
+     void initTexts(String jazyk){
+        Map<String,String> mapaString = new HashMap<>();
+            switch(jazyk) {
+                case "SK" :
+                    mapaString.put("uvod1", "Prihlásenie");
+                    mapaString.put("uvod2", "E-mail:");
+                    mapaString.put("otazka", "Heslo:");
+                    mapaString.put("zmazat", "Prihlasiť");
+                    mapaString.put("spat", "Späť");
+                    mapaString.put("potvrdit", "Upozornenie");
+                    mapaString.put("idPrazdneText", "Nezadali ste ID zamestnanca!");
+                    mapaString.put("idPrazdneNadpis", "Upozornenie");
+                    mapaString.put("idNeexistujuceText", "Zamestnanec s daným ID neexistuje!");
+                    mapaString.put("idNeexistujuceNadpis", "Upozornenie");
+                    mapaString.put("zmazanyUpozornenieText", "Zamestnanec bol zmazaný.");
+                    mapaString.put("zmazanyUpozornenieNadpis", "OK");
+                    mapaString.put("neoznacenyUpozornenieText", "Neoznačili ste zamestnanca!");
+                    mapaString.put("neoznacenyUpozornenieNadpis", "Upozornenie");
+                    break;
+                case "EN" :
+                    mapaString.put("uvod1", "");
+                    mapaString.put("uvod2", "");
+                    mapaString.put("otazka", "");
+                    mapaString.put("zmazat", "");
+                    mapaString.put("spat", "");
+                    mapaString.put("potvrdit", "");
+                    mapaString.put("idPrazdneText", "");
+                    mapaString.put("idPrazdneNadpis", "");
+                    mapaString.put("idNeexistujuceText", "");
+                    mapaString.put("idNeexistujuceNadpis", "");
+                    mapaString.put("zmazanyUpozornenieText", "");
+                    mapaString.put("zmazanyUpozornenieNadpis", "");
+                    mapaString.put("neoznacenyUpozornenieText", "");
+                    mapaString.put("neoznacenyUpozornenieNadpis", "");
+                    break;
+                
+            }
+            this.mapaString = mapaString;
+    }
+    
+    void nastavJazyk() {
+        initTexts(Nastavenia.jazyk);
+        
+        zmazatZamestnancaLabel.setText(mapaString.get("uvod1"));
+        zadatIDZamestnancaLabel.setText(mapaString.get("uvod2"));
+        overovaciaOtazkaLabel.setText(mapaString.get("otazka"));
+        zmazatButton.setText(mapaString.get("zmazat"));
+        spatButton.setText(mapaString.get("spat"));
+        potvrditButton.setText(mapaString.get("potvrdit"));
+      
     }
     
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        zmazatZamestnancaLabel = new javax.swing.JLabel();
+        zadatIDZamestnancaLabel = new javax.swing.JLabel();
         IDZamestnancaTextField = new javax.swing.JTextField();
         zmazatButton = new javax.swing.JButton();
         overovaciaOtazkaLabel = new javax.swing.JLabel();
@@ -43,11 +101,11 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
 
         setBackground(new java.awt.Color(255, 250, 226));
 
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
-        jLabel1.setText("Zmazať zamestnanca");
+        zmazatZamestnancaLabel.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        zmazatZamestnancaLabel.setText("Zmazať zamestnanca");
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 15)); // NOI18N
-        jLabel2.setText("Zadajte ID zamestnanca:");
+        zadatIDZamestnancaLabel.setFont(new java.awt.Font("Tahoma", 0, 15)); // NOI18N
+        zadatIDZamestnancaLabel.setText("Zadajte ID zamestnanca:");
 
         IDZamestnancaTextField.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyReleased(java.awt.event.KeyEvent evt) {
@@ -110,8 +168,8 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
                     .addComponent(overovaciaOtazkaLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(zmazatZamestnancaLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(zadatIDZamestnancaLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(zmenaJazykaPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
@@ -126,9 +184,9 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(zmenaJazykaPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(zmazatZamestnancaLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel2)
+                .addComponent(zadatIDZamestnancaLabel)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(IDZamestnancaTextField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
@@ -147,7 +205,7 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
 
     private void zmazatButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_zmazatButtonActionPerformed
         if(IDZamestnancaTextField.getText().equals("")){
-            JOptionPane.showMessageDialog(parentJForm, "Nezadali ste ID zamestnanca!", "Upozornenie", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(parentJForm, mapaString.get("idPrazdneText"), mapaString.get("idPrazdneNadpis"), JOptionPane.ERROR_MESSAGE);
         }else{
             Long zadaneID = Long.parseLong(IDZamestnancaTextField.getText());
             try{
@@ -162,7 +220,7 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
                 jScrollPane2.setVisible(true);
                 potvrditButton.setVisible(true);
             }catch(Exception e){
-                JOptionPane.showMessageDialog(parentJForm, "Zamestnanec s daným ID neexistuje!", "Upozornenie", JOptionPane.ERROR_MESSAGE);    
+                JOptionPane.showMessageDialog(parentJForm, mapaString.get("idNeexistujuceText"), mapaString.get("idNeexistujuceNadpis"), JOptionPane.ERROR_MESSAGE);    
             }
         }
         
@@ -183,14 +241,14 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
     private void potvrditButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_potvrditButtonActionPerformed
         if((boolean)zamestnanecNaMazanieTable.getValueAt(0, 3)){
             parentJForm.zmazZamestnanca(zamestnanec);
-            JOptionPane.showMessageDialog(parentJForm, "Zamestnanec bol zmazaný.", "OK", JOptionPane.PLAIN_MESSAGE);
+            JOptionPane.showMessageDialog(parentJForm, mapaString.get("zmazanyUpozornenieText"), mapaString.get("zmazanyUpozornenieNaspis"), JOptionPane.PLAIN_MESSAGE);
             IDZamestnancaTextField.setText("");
             overovaciaOtazkaLabel.setVisible(false);  
             jScrollPane2.setVisible(false);
             potvrditButton.setVisible(false);
             model.setHodnota(false);
         }else{
-            JOptionPane.showMessageDialog(parentJForm, "Neoznačili ste zamestnanca!", "Upozornenie", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(parentJForm, mapaString.get("neoznacenyUpozornenieText"), mapaString.get("neoznacenyUpozornenieNadpis"), JOptionPane.ERROR_MESSAGE);
         }
     }//GEN-LAST:event_potvrditButtonActionPerformed
 
@@ -213,18 +271,16 @@ public class ZmazatZamestnanca extends javax.swing.JPanel {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JTextField IDZamestnancaTextField;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel overovaciaOtazkaLabel;
     private javax.swing.JButton potvrditButton;
     private javax.swing.JButton spatButton;
+    private javax.swing.JLabel zadatIDZamestnancaLabel;
     private javax.swing.JTable zamestnanecNaMazanieTable;
     private javax.swing.JButton zmazatButton;
+    private javax.swing.JLabel zmazatZamestnancaLabel;
     private com.paz1c.gui.PrihlasenieRegistracia.zmenaJazykaPanel zmenaJazykaPanel1;
     // End of variables declaration//GEN-END:variables
 
-    void nastavJazyk() {
     
-    }
 }
